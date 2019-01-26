4100e1e20093b7ce3fe0385ffb57b17a947afaac
Thomas Stoeckert <Thomas@Stoeckert.us>
2019-01-25 20:09:28 -0500

    Added info/settings page to the main user menu

--- lib/widgets/back_button.dart
+++ lib/widgets/back_button.dart
@@ -2,35 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../animations/fade_in_widget.dart';
 
+enum BackButtonDirection {
+  LEFT,
+  RIGHT
+}
+
 class RoundBackButton extends StatelessWidget {
+  RoundBackButton({this.direction = BackButtonDirection.LEFT});
+
+  final BackButtonDirection direction;
+
   @override
   Widget build(BuildContext context) {
-    return FadeInWidget(
-      child: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(24.0),
-              boxShadow: <BoxShadow>[
-                BoxShadow(
-                  color: Colors.grey[900],
-                  offset: Offset(0, 1),
-                  blurRadius: 3.0
-                )
-              ]
-            ),
-            child: Material(
-              color: Theme.of(context).primaryColor,
-              borderRadius: BorderRadius.circular(24.0),
-              child: IconButton(
-                alignment: Alignment.topLeft,
-                icon: Icon(
-                  FontAwesomeIcons.arrowLeft,
-                  color: Colors.white,
-                  size: 32.0,
+    bool left = (direction == BackButtonDirection.LEFT);
+    return Align(
+      alignment: left ? Alignment.topLeft : Alignment.topRight,
+      child: FadeInWidget(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(24.0),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    color: Colors.grey[900],
+                    offset: Offset(0, 1),
+                    blurRadius: 3.0
+                  )
+                ]
+              ),
+              child: Material(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(24.0),
+                child: IconButton(
+                  icon: Icon(
+                    left ? FontAwesomeIcons.arrowLeft : FontAwesomeIcons.arrowRight,
+                    color: Colors.white,
+                    size: 32.0,
+                  ),
+                  onPressed: () => Navigator.of(context).pop(),
                 ),
-                onPressed: () => Navigator.of(context).pop(),
               ),
             ),
           ),
