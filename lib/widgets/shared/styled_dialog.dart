c0e622eabdaa9711e4c00038373b5ff282d4821e
Thomas Stoeckert <Thomas@Stoeckert.us>
2019-04-22 14:25:23 -0400

    Updated the Styled Dialog to allow for further user actions.

--- lib/widgets/shared/styled_dialog.dart
+++ lib/widgets/shared/styled_dialog.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/material.dart';
 
 class StyledDialog extends StatelessWidget {
-  StyledDialog({this.title, this.body, this.actionText});
+  StyledDialog({this.title, this.body, this.actionText, this.additionalAction});
 
   final String title;
   final String body;
   final String actionText;
+  final Widget additionalAction;
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> buttons = List<Widget>();
+    buttons.add(
+        FlatButton(
+          child: Text(actionText),
+          onPressed: () => Navigator.of(context).pop()
+    ));
+
+    if(additionalAction != null){
+      buttons.add(additionalAction);
+    }
+
     return AlertDialog(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
       title: Text(title),
       content: Text(body),
-      actions: <Widget>[
-        FlatButton(
-          child: Text(actionText),
-          onPressed: () => Navigator.of(context).pop(),
-        )
-      ],
+      actions: buttons,
     );
   }
 }
